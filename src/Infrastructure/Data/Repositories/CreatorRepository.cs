347dc4a12a9b32cd500eafb2a8441c598d8c5fa2
Dylan Hackworth <me@dylhack.dev>
2025-04-07 05:51:40 -0500

    data: reverse order of creator transactions

--- src/Infrastructure/Data/Repositories/CreatorRepository.cs
+++ src/Infrastructure/Data/Repositories/CreatorRepository.cs
@@ -9,9 +9,9 @@ public class CreatorRepository(ApplicationDbContext context) : RepositoryBase<Cr
     public Task<Creator[]> GetAll() => DbSet.ToArrayAsync();
     public Task<Creator[]> GetAllByIds(int[] ids) => DbSet.Where(c => ids.Contains(c.Id)).ToArrayAsync();
     public Task<Creator?> GetDetails(string slug) => DbSet
-            .Include(c => c.Transactions)
+            .Include(c => c.Transactions.OrderByDescending(t => t.CreatedAt))
             .ThenInclude(t => t.User)
-            .Include(c => c.Transactions).FirstOrDefaultAsync(c => c.Slug == slug);
+            .FirstOrDefaultAsync(c => c.Slug == slug);
     public Task<Creator[]> GetAllAbove(long value) => DbSet.Where(c => c.Value > value).ToArrayAsync();
     public Task<long> GetValueSum() => DbSet.SumAsync(c => c.Value);
     public async Task<Creator?> UpdateStreamInfo(int id, StreamStatus status)
