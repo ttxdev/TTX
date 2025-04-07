c9378b5841dd008c81bedc74f876a6f67a623f08
Dylan Hackworth <me@dylhack.dev>
2025-04-07 08:54:16 -0500

    core: added queue to StreamMonitorService

--- src/Core/Services/StreamMonitorService.cs
+++ src/Core/Services/StreamMonitorService.cs
@@ -1,4 +1,5 @@
 using TTX.Core.Interfaces;
+using TTX.Core.Models;
 using TTX.Core.Repositories;
 
 namespace TTX.Core.Services;
@@ -6,15 +7,30 @@ namespace TTX.Core.Services;
 public interface IStreamMonitorService
 {
     Task Start(CancellationToken token);
+    void AddCreator(Creator creator);
+    void RemoveCreator(Creator creator);
 }
 
-public class StreamMonitorService(IStreamService streamService, ICreatorRepository creatorRepository) : IStreamMonitorService
+public class StreamMonitorService : IStreamMonitorService
 {
+    private readonly IStreamService streamService;
+    private readonly ICreatorRepository creatorRepository;
+    private readonly Queue<StreamStatusUpdate> streamStatusQueue = new();
+    public StreamMonitorService(IStreamService streamService, ICreatorRepository creatorRepository)
+    {
+        this.streamService = streamService;
+        this.creatorRepository = creatorRepository;
+        streamService.OnStreamUpdate += (s, e) => streamStatusQueue.Enqueue(e);
+    }
+
+    public void AddCreator(Creator creator) => streamService.AddCreator(creator);
+    public void RemoveCreator(Creator creator) => streamService.RemoveCreator(creator);
     public async Task Start(CancellationToken token)
     {
-        streamService.OnStreamUpdate += async (s, e) => await creatorRepository.UpdateStreamInfo(e.CreatorId, e);
         await streamService.Start(token);
-
-        while (!token.IsCancellationRequested) { }
+        while (!token.IsCancellationRequested) {
+            if (streamStatusQueue.TryDequeue(out var statusUpdate))
+                await creatorRepository.UpdateStreamInfo(statusUpdate.CreatorId, statusUpdate);
+        }
     }
 }
