cef0ee63e129086a5a97898573898ec422e3d85f
Dylan Hackworth <me@dylhack.dev>
2025-04-07 04:16:39 -0500

    api: CreatorDto should return CreatorShares

--- src/Interface/Api/Dto/CreatorDto.cs
+++ src/Interface/Api/Dto/CreatorDto.cs
@@ -8,5 +8,5 @@ public class CreatorDto(Creator creator) : CreatorPartialDto(creator)
     [JsonPropertyName("transactions")]
     public CreatorTransactionDto[] Transactions { get; } = [.. creator.Transactions.Select(x => new CreatorTransactionDto(x))];
     [JsonPropertyName("shares")]
-    public UserShareDto[] Shares { get; } = [.. creator.GetShares().Select(x => new UserShareDto(x))];
+    public CreatorShareDto[] Shares { get; } = [.. creator.GetShares().Select(x => new CreatorShareDto(x))];
 }
