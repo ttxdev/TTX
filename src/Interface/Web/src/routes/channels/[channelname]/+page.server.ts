7e009691eaf57b13b72340c3ce9f69f400d7ceff
Ruben Olano <76502130+RubenOlano@users.noreply.github.com>
2025-04-09 12:42:02 -0500

    Time Selector (#4)

--- src/Interface/Web/src/routes/channels/[channelname]/+page.server.ts
+++ src/Interface/Web/src/routes/channels/[channelname]/+page.server.ts
@@ -2,21 +2,33 @@ import { getApiClient } from '$lib';
 import { getToken } from '$lib/auth';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types.js';
-import { CreatorDto, TimeStep, Vote } from '$lib/api.js';
+import { CreatorDto, CreatorShareDto, CreatorTransactionDto, TimeStep } from '$lib/api.js';
 
-/** @type {import('./$types').PageServerLoad} */
-export const load: PageServerLoad = async ({ cookies, params }) => {
+export type Interval = 'all' | '24h' | '12h' | '6h' | '1h'
+
+export const load: PageServerLoad = async ({ cookies, params, url }) => {
 	try {
 		const channelSlug = params.channelname.toLowerCase();
+		const interval = (url.searchParams.get('interval') || '1h') as Interval;
 
 		const token = getToken(cookies);
 		const client = getApiClient(token || '');
 		const creator = await client.getCreator(channelSlug);
 
+		const hours = interval === 'all' ? 1000000 : interval === '24h' ? 24 : interval === '12h' ? 12 : interval === '6h' ? 6 : 1;
+
+
+		const shareHistory = await client.getLatestCreatorValue(channelSlug, new Date(Date.now() - hours * 60 * 60 * 1000), TimeStep.Minute);
+
+
 		return {
-			creator: creator.toJSON(),
-			shares: creator.shares.map((d) => d.toJSON()),
-			transactions: creator.transactions.map((d) => d.toJSON())
+			creator: {
+				...creator.toJSON(),
+				history: shareHistory.map((d) => d.toJSON())
+			} as CreatorDto,
+			shares: creator.shares.map((d) => d.toJSON() as CreatorShareDto),
+			transactions: creator.transactions.map((d) => d.toJSON() as CreatorTransactionDto),
+			interval
 		};
 	} catch (err) {
 		console.error(err);
