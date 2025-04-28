d3396a8afdafbd4c1458a1583e9be59aba34c040
Dylan Hackworth <me@dylhack.dev>
2025-04-28 14:07:16 -0500

    web: pass orderDir to creator index

--- src/Web/src/routes/creators/+page.server.ts
+++ src/Web/src/routes/creators/+page.server.ts
@@ -7,7 +7,7 @@ export const load: PageServerLoad = async ({ cookies, url }) => {
 	const client = getApiClient(getToken(cookies) ?? '');
 	const page = Number(url.searchParams.get('page')) || 1;
 	const orderBy = url.searchParams.get('orderBy') || 'Name';
-	const order =
+	const orderDir =
 		url.searchParams.get('order') == 'desc' ? OrderDirection.Descending : OrderDirection.Ascending;
 	const search = url.searchParams.get('search') || '';
 
@@ -24,14 +24,14 @@ export const load: PageServerLoad = async ({ cookies, url }) => {
 			break;
 	}
 
-	const creators = await client.getCreators(page, 20, search, safeOrderBy);
+	const creators = await client.getCreators(page, 20, search, safeOrderBy, orderDir);
 
 	return {
 		channels: creators.data.map((c) => c.toJSON()),
 		total: creators.total,
 		currentPage: page,
 		sortField: orderBy,
-		sortDirection: order,
+		sortDirection: orderDir,
 		searchQuery: search
 	};
 };
