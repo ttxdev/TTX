71587435270dba37d5009f25ec17e34e78997cb0
Dylan Hackworth <me@dylhack.dev>
2025-05-02 22:06:41 -0500

    web: rename /leaderboard to /players

--- src/Web/src/routes/players/+page.server.ts
+++ src/Web/src/routes/players/+page.server.ts
@@ -1,7 +1,37 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { getApiClient } from '$lib';
+import { PlayerOrderBy, OrderDirection } from '$lib/api';
 
-export const load = (async () => {
-	redirect(302, '/');
-	return {};
-}) satisfies PageServerLoad;
+export const load: PageServerLoad = async ({ url }) => {
+    const client = getApiClient('');
+    const page = Number(url.searchParams.get('page')) || 1;
+    const search = url.searchParams.get('search') || ''; // Get the search query
+    const orderBy = url.searchParams.get('orderBy') || 'Portfolio';
+    const orderDir =
+        url.searchParams.get('order') === 'asc' ? OrderDirection.Ascending : OrderDirection.Descending;
+
+    let safeOrderBy: PlayerOrderBy;
+    switch (orderBy) {
+        case 'Name':
+            safeOrderBy = PlayerOrderBy.Name;
+            break;
+        case 'Credits':
+            safeOrderBy = PlayerOrderBy.Credits;
+            break;
+        default:
+            safeOrderBy = PlayerOrderBy.Portfolio;
+            break;
+    }
+
+    // Pass the search parameter to the API call
+    const players = await client.getPlayers(page, 20, search, safeOrderBy, orderDir);
+
+    return {
+        players: players.data.map((p) => p.toJSON()),
+        total: players.total,
+        currentPage: page,
+        sortField: orderBy,
+        sortDirection: orderDir,
+        searchQuery: search // Return the search query for client-side use
+    };
+};
