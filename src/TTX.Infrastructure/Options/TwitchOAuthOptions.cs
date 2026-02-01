3f4c2b2dd3c2c20ba1b230bebba499534f4cc12b
Dylan Hackworth <me@dylhack.dev>
2026-02-01 15:58:34 -0600

    infra: separate build images

--- src/TTX.Infrastructure/Options/TwitchOAuthOptions.cs
+++ src/TTX.Infrastructure/Options/TwitchOAuthOptions.cs
@@ -5,5 +5,5 @@ public sealed class TwitchOAuthOptions
     public required string ClientId { get; init; }
     public required string ClientSecret { get; init; }
     public required string RedirectUri { get; init; }
-    public string[] Scopes { get; init; } = [];
+    public required string[] Scopes { get; init; }
 }
