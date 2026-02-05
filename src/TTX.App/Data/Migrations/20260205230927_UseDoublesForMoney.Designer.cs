5e4ce83ed5fe6244474692fc07978c888dcc932b
Dylan Hackworth <me@dylhack.dev>
2026-02-05 17:15:28 -0600

    app: add migrations

--- src/TTX.App/Data/Migrations/20260205230927_UseDoublesForMoney.Designer.cs
+++ src/TTX.App/Data/Migrations/20260205230927_UseDoublesForMoney.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TTX.App.Data;
@@ -11,9 +12,11 @@ using TTX.App.Data;
 namespace TTX.App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260205230927_UseDoublesForMoney")]
+    partial class UseDoublesForMoney
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TTX.App/Data/Migrations/20260205230927_UseDoublesForMoney.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TTX.App.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseDoublesForMoney : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "value",
+                schema: "public",
+                table: "votes",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "value",
+                schema: "public",
+                table: "transactions",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "portfolio",
+                schema: "public",
+                table: "players",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "credits",
+                schema: "public",
+                table: "players",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "value",
+                schema: "public",
+                table: "player_portfolios",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "value",
+                schema: "public",
+                table: "creators",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "value",
+                schema: "public",
+                table: "votes",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "value",
+                schema: "public",
+                table: "transactions",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "portfolio",
+                schema: "public",
+                table: "players",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "credits",
+                schema: "public",
+                table: "players",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "value",
+                schema: "public",
+                table: "player_portfolios",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "value",
+                schema: "public",
+                table: "creators",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
