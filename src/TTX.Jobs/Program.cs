019396d5f4fc6d3361973cfda31d06a8cc4f97ef
Dylan Hackworth <me@dylhack.dev>
2026-03-11 17:53:57 -0500

    jobs: added otel

--- src/TTX.Jobs/Program.cs
+++ src/TTX.Jobs/Program.cs
@@ -1,5 +1,9 @@
 using TTX.App;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 using TTX.Infrastructure;
+using OpenTelemetry.Resources;
 
 IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -10,6 +14,14 @@ IConfiguration configuration = new ConfigurationBuilder()
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services
+    .AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddHostDetector().AddContainerDetector())
+    .WithLogging(logging => logging.AddOtlpExporter())
+    .WithTracing(tracing => tracing.AddOtlpExporter())
+    .WithMetrics(metrics => metrics
+        .AddRuntimeInstrumentation()
+        .AddOtlpExporter())
+    .Services
     .AddTtx(configuration.GetSection("TTX"))
     .AddTtxJobs(configuration.GetSection("TTX:Jobs"))
     .AddTtxInfra(configuration.GetSection("TTX:Infrastructure"))
